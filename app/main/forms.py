d86bc2b689c47eb6968d50fe4e97e6a616e15034
colletemine <jepngetichcolleta@gmail.com>
2022-05-15 17:53:13 +0300

    fix:debug main forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,29 @@
-from turtle import title
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileField,FileAllowed
+from wtforms import StringField, TextAreaField, SubmitField,ValidationError
+from wtforms.validators import DataRequired,InputRequired,Email
+from flask_login import current_user
 from wtforms.widgets import TextArea
+from ..models import User
 
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
-    submit = SubmitField('Save')
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('Address', validators=[InputRequired(),Email()])
+    bio = TextAreaField('Bio',validators = [InputRequired()])
+    profile_picture = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("The username has already been taken")
+
 
 
 
