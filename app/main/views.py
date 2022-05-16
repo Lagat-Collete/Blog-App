35cbfeb68f155092b688ef52dfa7b65ff368100e
colletemine <jepngetichcolleta@gmail.com>
2022-05-16 14:06:27 +0300

    fix:update profle.html file

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required,current_user
 from requests import get_quotes
 from app.email import mail_message
-from app.main.forms import PostForm, createPost
+from app.main.forms import PostForm, UpdateProfile, createPost
 from .. import db,photos
 from . import main
 from .. models import Comment, Post, Subscriber, User
@@ -18,7 +18,44 @@ def index():
     quotes = get_quotes()
     return render_template("index.html",quote = quotes)
 
-@main.route('/new-post', methods=['GET','POST'])
+
+@main.route('/profile',methods = ['POST','GET'])
+# @login_required
+def profile():
+    form = UpdateProfile()
+    profile_pic_path = ''
+    print('CURRENT', current_user)
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            current_user.bio = form.bio.data
+            db.session.commit()
+            flash('Succesfully updated your profile')
+            return redirect(url_for('profile'))
+        elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.email.data = current_user.email
+            form.bio.data = current_user.bio
+            profile_pic_path = url_for('static',filename ='images/'+ current_user.profile_pic_path) 
+    return render_template('profile.html', profile_pic_path = profile_pic_path, form = form)    
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('updateprofile.html',form =form)
+
+
+
+@main.route('/new_post', methods=['GET','POST'])
 def new_post():
   form = PostForm()
 
